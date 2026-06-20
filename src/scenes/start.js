6fd60e07b198586b592cb4c8af0d8163fd5d9fda
Erick Guedes <erick.guedes@gmail.com>
2026-06-20 16:57:52 -0300

    including a 3d model

--- src/scenes/start.js
+++ src/scenes/start.js
@@ -1,4 +1,5 @@
-import {Scene, HemisphericLight, FreeCamera, Vector3, MeshBuilder, WebXRHitTest, WebXRAnchorSystem} from 'babylonjs';
+import {Scene, HemisphericLight, FreeCamera, Vector3, MeshBuilder,
+    WebXRHitTest, WebXRAnchorSystem, PointerEventTypes, SceneLoader} from 'babylonjs';
 
 export async function startScene(engine) {
     const scene = new Scene(engine);
@@ -13,18 +14,35 @@ export async function startScene(engine) {
     dot.material = new BABYLON.StandardMaterial('dotMat', scene);
     dot.material.diffuseColor.set(1, 0, 0);
 
+    const {meshes, animationGroups} = await SceneLoader.ImportMeshAsync('', './models/', 'capoeira.glb', scene)
+    
+    meshes[0].position.x = 2
+
     const xr =await scene.createDefaultXRExperienceAsync({
         uiOptions: { sessionMode: 'immersive-ar' }
     })
 
     const fm = xr.baseExperience.featuresManager;
     const hitTest = fm.enableFeature(WebXRHitTest, 'latest');
+    const anchorSystem = fm.enableFeature(WebXRAnchorSystem, 'latest');
 
+    let lastHit = undefined
     hitTest.onHitTestResultObservable.add((results) => {
         if(results.length) {
+            lastHit = results[0]
             results[0].transformationMatrix.decompose(dot.scaling, dot.rotationQuaternion, dot.position);
         }
-    });
+    })
+    anchorSystem.onAnchorAddedObservable.add( anchor => {
+       //anchor.attachedNode = dot.clone()
+       const clone = meshes[0].clone()
+       //clone.position.x = Scalar.RandomRange(1.5, 2)
+       anchor.attachedNode = clone
+    })
+
+    scene.onPointerObservable.add( event => {
+        if(lastHit && anchorSystem) anchorSystem.addAnchorPointUsingHitTestResultAsync(lastHit)
+        }, PointerEventTypes.POINTERDOWN)
 
     await scene.whenReadyAsync();
     return scene;
